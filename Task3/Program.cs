e85ef143c64822ae69725a919f10878b112e5ae3
Tatiana <zanka_16@mail.ru>
2023-02-13 20:15:36 +0800

    Решение задания 3 ДЗ9

---
+++ Task3/Program.cs
@@ -0,0 +1,35 @@
+﻿/*Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
+m = 2, n = 3 -> A(m,n) = 9
+m = 3, n = 2 -> A(m,n) = 29
+*/
+int m = Input("Введите M ");
+int n = Input("Введите N ");
+
+if (n < 0 || m < 0) Console.Write($"введите положительные числа");
+else
+{
+    Console.WriteLine($"Функция Аккермана для чисел M({m},{n}) = {Akk(m, n)}");
+}
+
+int Akk(int m, int n)
+{
+    if (m == 0)
+    {
+        return n + 1;
+    }
+    else if (n == 0)
+    {
+
+        return Akk(m - 1, 1);
+    }
+    else
+    {
+        return Akk(m - 1, Akk(m, n - 1));
+    }
+}
+int Input(string input)
+{
+    Console.Write(input);
+    int output = Convert.ToInt32(Console.ReadLine());
+    return output;
+}
